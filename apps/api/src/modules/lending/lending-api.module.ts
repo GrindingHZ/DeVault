5c5b9087ad748270f5e6c304d104b8a66b3a1925
GrindingHZ <zhen0016@student.monash.edu>
2026-09-02 16:33:33 +0800

    feat(lending): list, withdraw, purchase, and void note sales

--- apps/api/src/modules/lending/lending-api.module.ts
+++ apps/api/src/modules/lending/lending-api.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { CUSTODY_RECEIPT_REPOSITORY } from '../../domain/custody/custody-receipt-repository';
 import { LIQUIDATION_REPOSITORY } from '../../domain/lending/liquidation-repository';
 import { LOAN_REPOSITORY } from '../../domain/lending/loan-repository';
+import { NOTE_SALE_REPOSITORY } from '../../domain/lending/note-sale-repository';
 import { LISTING_REPOSITORY } from '../../domain/marketplace/listing-repository';
 import { LOAN_QUERIES } from '../../domain/ports/loan-queries.port';
 import { PrismaCustodyReceiptRepository } from '../../infrastructure/persistence/repositories/prisma-custody-receipt.repository';
 import { PrismaListingRepository } from '../../infrastructure/persistence/repositories/prisma-listing.repository';
 import { PrismaLiquidationRepository } from '../../infrastructure/persistence/repositories/prisma-liquidation.repository';
 import { PrismaLoanRepository } from '../../infrastructure/persistence/repositories/prisma-loan.repository';
+import { PrismaNoteSaleRepository } from '../../infrastructure/persistence/repositories/prisma-note-sale.repository';
 import { PrismaLoanQueries } from '../../infrastructure/persistence/queries/prisma-loan-queries';
 import { AcceptOfferUseCase } from './application/accept-offer.use-case';
+import { ListNoteForSaleUseCase } from './application/list-note-for-sale.use-case';
+import { PurchaseNoteSaleUseCase } from './application/purchase-note-sale.use-case';
+import { WithdrawNoteSaleUseCase } from './application/withdraw-note-sale.use-case';
 import { ClaimReceiptUseCase } from './application/claim-receipt.use-case';
 import { MarkDefaultUseCase } from './application/mark-default.use-case';
 import { PayoffQuoteQuery } from './application/payoff-quote.query';
@@ -37,10 +42,14 @@ import { LiquidationController } from './http/liquidation.controller';
     CloseLiquidationUseCase,
     ReclaimBidUseCase,
     LiquidationQuery,
+    ListNoteForSaleUseCase,
+    WithdrawNoteSaleUseCase,
+    PurchaseNoteSaleUseCase,
     { provide: LIQUIDATION_REPOSITORY, useClass: PrismaLiquidationRepository },
     { provide: LISTING_REPOSITORY, useClass: PrismaListingRepository },
     { provide: CUSTODY_RECEIPT_REPOSITORY, useClass: PrismaCustodyReceiptRepository },
     { provide: LOAN_REPOSITORY, useClass: PrismaLoanRepository },
+    { provide: NOTE_SALE_REPOSITORY, useClass: PrismaNoteSaleRepository },
     { provide: LOAN_QUERIES, useClass: PrismaLoanQueries },
   ],
 })
